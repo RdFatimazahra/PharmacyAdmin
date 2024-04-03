437f013bf70ebc3e7deaa9e39b002f412fca1180
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-04-03 14:02:30 +0000

    Fichier SQL : Insertion et Affichage

---
+++ InsertionAndAffichage.sql
@@ -0,0 +1,9 @@
+INSERT INTO product (nom, quantite, description, prix) 
+VALUES 
+  ('Product 1', 15, 'Description 1', 149.99),
+  ('Product 2', 20, 'Description 2', 199.99),
+  ('Product 3', 22, 'Description 3', 200),
+  ('Product 4', 30, 'Description 4', 259.99);
+  
+  select * from product
+  
