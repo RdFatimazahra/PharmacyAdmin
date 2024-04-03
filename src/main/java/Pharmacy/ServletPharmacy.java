de932faae60e1018a36d7986da5ac58b4af887ff
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-04-03 15:57:22 +0000

    Création du ServletPharmacy

---
+++ src/main/java/Pharmacy/ServletPharmacy.java
@@ -0,0 +1,23 @@
+package Pharmacy;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ServletPharmacy extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+				request.getRequestDispatcher("file.jsp").forward(request, response);
+	}
+
+	
+	/*
+	 * protected void doPost(HttpServletRequest request, HttpServletResponse
+	 * response) throws ServletException, IOException { // TODO Auto-generated
+	 * method stub doGet(request, response); }
+	 */
+
+}
